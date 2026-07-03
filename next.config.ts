edf26d68a7997743a66188e6cadb5b210d2ab885
Ryan <Redninja106@live.com>
2026-07-03 13:23:12 -0400

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   reactCompiler: true,
+  trailingSlash: true,
+  basePath: "/cmac",
   turbopack: {
     root: process.cwd(),
   },
